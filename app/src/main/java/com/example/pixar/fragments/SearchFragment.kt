61a4a1330e15eeabd5dc99369ad2fb04b6561924
yash <yashkumar201301@gmail.com>
2021-06-21 23:30:56 +0530

    basic setup

---
+++ app/src/main/java/com/example/pixar/fragments/SearchFragment.kt
@@ -0,0 +1,14 @@
+package com.example.pixar.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pixar.R
+
+class SearchFragment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_search)
+
+    }
+}
