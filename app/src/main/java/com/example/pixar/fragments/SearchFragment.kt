3139c213012e271607c0ef6b3efccff88932a7cc
yash <yashkumar201301@gmail.com>
2021-06-24 15:08:56 +0530

    search fragment layout

--- app/src/main/java/com/example/pixar/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/pixar/fragments/SearchFragment.kt
@@ -1,14 +1,33 @@
 package com.example.pixar.fragments
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.pixar.R
+import com.example.pixar.databinding.FragmentSearchBinding
 
-class SearchFragment : AppCompatActivity() {
+class SearchFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_search)
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
--- app/src/main/java/com/example/pixar/model/UnsplashPhoto.kt
+++ app/src/main/java/com/example/pixar/model/UnsplashPhoto.kt
@@ -6,7 +6,6 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class UnsplashPhoto(
     val id: String,
-    val description: String?,
     val urls: UnsplashPhotoUrls,
 ) : Parcelable {
 
