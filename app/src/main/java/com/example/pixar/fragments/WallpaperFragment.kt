579761edb7fe41de5202a4f4da8c36aca19a5d09
yash <yashkumar201301@gmail.com>
2021-07-01 14:47:00 +0530

    fixed connection issue in WallpaperFragment.kt

--- app/src/main/java/com/example/pixar/fragments/WallpaperFragment.kt
+++ app/src/main/java/com/example/pixar/fragments/WallpaperFragment.kt
@@ -2,15 +2,21 @@ package com.example.pixar.fragments
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.pixar.R
 import com.example.pixar.databinding.FragmentWallpaperBinding
 import com.example.pixar.utils.Constants.Companion.UNSPLASH_URL
@@ -41,10 +47,33 @@ class WallpaperFragment : Fragment() {
             Glide.with(this@WallpaperFragment)
                 .load(photo.urls.regular)
                 .centerCrop()
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        imageError.isVisible = true
+                        textError.isVisible = true
+                        return false
+                    }
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        imageError.isVisible = false
+                        textError.isVisible = false
+                        return false
+                    }
+                })
                 .transition(DrawableTransitionOptions.withCrossFade())
                 .into(binding.imageWallpaper)
 
-            textLikes.text = photo.likes.toString().plus("Likes")
+            textLikes.text = photo.likes.toString().plus(" Likes")
 
             textUsername.paint.isUnderlineText = true
             textUsername.text = photo.user.username
