4464faf589da971b026da54ac8b8fe133dedbb67
yash <yashkumar201301@gmail.com>
2021-07-10 16:26:47 +0530

    search fragment and category fragment separated

--- app/src/main/java/com/example/pixar/fragments/SearchWallpaperFragment.kt
+++ app/src/main/java/com/example/pixar/fragments/SearchWallpaperFragment.kt
@@ -3,35 +3,34 @@ package com.example.pixar.fragments
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.pixar.R
 import com.example.pixar.adapters.UnsplashPhotoAdapter
-import com.example.pixar.databinding.FragmentSearchBinding
+import com.example.pixar.databinding.FragmentSearchWallpaperBinding
 import com.example.pixar.model.UnsplashPhoto
 import com.example.pixar.paging.UnsplashLoadStateAdapter
 import com.example.pixar.viewmodel.ImagesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SearchFragment : Fragment(), UnsplashPhotoAdapter.OnItemClickListener {
+class SearchWallpaperFragment : Fragment(), UnsplashPhotoAdapter.OnItemClickListener {
 
-    private var _binding: FragmentSearchBinding? = null
+    private var _binding: FragmentSearchWallpaperBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel by viewModels<ImagesViewModel>()
-
-    private val searchQuery by navArgs<SearchFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        _binding = FragmentSearchWallpaperBinding.inflate(inflater, container, false)
 
         // paging and footer adapter
         val adapter = UnsplashPhotoAdapter(this)
@@ -59,9 +58,6 @@ class SearchFragment : Fragment(), UnsplashPhotoAdapter.OnItemClickListener {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
 
-            binding.toolbar.title = searchQuery.query
-            viewModel.searchPhotos(searchQuery.query)
-
         adapter.addLoadStateListener { loadState ->
             binding.apply {
 
@@ -85,8 +81,32 @@ class SearchFragment : Fragment(), UnsplashPhotoAdapter.OnItemClickListener {
         }
 
         (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_gallery, menu)
+
+        val searchItem = menu.findItem(R.id.actionSearch)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.isSubmitButtonEnabled = true
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.recyclerView.scrollToPosition(0)
+                if (query != null) viewModel.searchPhotos(query)
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean = true
+        })
+
     }
 
     override fun onDestroyView() {
