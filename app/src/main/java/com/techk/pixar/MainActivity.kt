9a879dbc5e05d27a667399e4b6a72e8e7ee0bd8e
YashKr01 <yashkumar201301@gmail.com>
2022-05-14 10:43:20 +0530

    # New Splash Screen

--- app/src/main/java/com/techk/pixar/MainActivity.kt
+++ app/src/main/java/com/techk/pixar/MainActivity.kt
@@ -2,6 +2,7 @@ package com.techk.pixar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         lifecycleScope.launch(Dispatchers.IO) { MobileAds.initialize(this@MainActivity) { } }
