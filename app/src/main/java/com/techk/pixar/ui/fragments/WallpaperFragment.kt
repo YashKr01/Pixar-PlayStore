b6e618a40b98f455433f3c81b3f14752c8db388d
YashKr01 <yashkumar201301@gmail.com>
2022-05-23 22:52:28 +0530

    # Code refactoring

--- app/src/main/java/com/techk/pixar/ui/fragments/WallpaperFragment.kt
+++ app/src/main/java/com/techk/pixar/ui/fragments/WallpaperFragment.kt
@@ -1,8 +1,6 @@
 package com.techk.pixar.ui.fragments
 
-import android.app.WallpaperManager
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
@@ -11,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
@@ -20,19 +17,9 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
-import com.techk.pixar.R
 import com.techk.pixar.databinding.FragmentWallpaperBinding
 import com.techk.pixar.model.UnsplashPhoto
 import com.techk.pixar.utils.Constants.Companion.UNSPLASH_URL
-import com.techk.pixar.utils.Constants.Companion.imageToBitmap
-import com.techk.pixar.utils.Constants.Companion.isOnline
-import com.techk.pixar.utils.Constants.Companion.showSnackBar
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.IOException
 
 class WallpaperFragment : Fragment() {
 
@@ -84,56 +71,14 @@ class WallpaperFragment : Fragment() {
         binding.fabWallpaper.setOnClickListener {
 
             // if device version is enough
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                val action =
-                    WallpaperFragmentDirections
-                        .actionWallpaperFragmentToWallpaperBottomSheetFragment(photo)
-                findNavController().navigate(action)
-            } else {
-                // else set wallpaper on home screen
-                if (isOnline(requireContext())) {
-                    lifecycleScope.launch(Dispatchers.IO) {
-
-                        val bitmap: Bitmap? = imageToBitmap(photo.urls.regular)
-
-                        if (bitmap != null) {
-                            val e = setWallpaperOnHomeScreen(bitmap)
-                            if (e != null) withContext(Dispatchers.Main) {
-                                snackBar(e)
-                            } else {
-                                withContext(Dispatchers.Main) {
-                                    snackBar(getString(R.string.wallpaper_has_been_set))
-                                    withContext(Dispatchers.IO) { delay(1000L) }
-                                }
-                            }
-                        }
-
-                    }
-                } else showSnackBar(
-                    requireContext(),
-                    binding.root,
-                    getString(R.string.no_connection),
-                    Snackbar.LENGTH_SHORT
-                )
-
-            }
+            val action =
+                WallpaperFragmentDirections
+                    .actionWallpaperFragmentToWallpaperBottomSheetFragment(photo)
+            findNavController().navigate(action)
 
         }
 
     }
-
-    private fun setWallpaperOnHomeScreen(bitmap: Bitmap): String? {
-        val wallpaperManager = WallpaperManager.getInstance(requireContext())
-        return try {
-            wallpaperManager.setBitmap(bitmap)
-            null
-        } catch (e: IOException) {
-            e.message.toString()
-        }
-    }
-
-    private fun snackBar(message: String) =
-        showSnackBar(requireContext(), binding.root, message, Snackbar.LENGTH_SHORT)
 
     private fun loadImage(source: UnsplashPhoto) {
 
